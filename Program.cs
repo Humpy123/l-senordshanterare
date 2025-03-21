493dd7ee4a7d4a1bae87d9ac6ab92db862e14d0b
Humpy123 <oliver@wahrolen.se>
2025-03-21 11:36:33 +0100

    completed get and set

--- Program.cs
+++ Program.cs
@@ -151,18 +151,33 @@ namespace lösenordshanterare
 
 
                 Dictionary<string, string> vaultData = JsonSerializer.Deserialize<Dictionary<string, string>>(decryptedVault);
-                Console.WriteLine("Enter password for new property)");
-                vaultData[property] = Console.ReadLine();
+
+                string password;
+                if (args.Length == 5/* && args[4] == "-g" && args[4] == "-generate"*/)
+                {
+                    if (args[4] == "-g" && args[4] == "-generate")
+                    {
+                        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                        Random random = new Random();
+                        vaultData[property] = new string(Enumerable.Repeat(chars, 20)
+                                                   .Select(s => s[random.Next(s.Length)])
+                                                   .ToArray());
+                    }
+
+                    else
+                        Console.WriteLine("Invalid flag");
+                }
+
+                else
+                {
+                    Console.WriteLine("Enter password for new property)");
+                    vaultData[property] = Console.ReadLine();
+                }                
 
                 string updatedVaultJson = JsonSerializer.Serialize(vaultData);
                 byte[] updatedEncryptedVault = CryptoHelper.Encrypt(updatedVaultJson, vaultKey, iv);
                 serv.PasswordVault = Convert.ToBase64String(updatedEncryptedVault);
 
-                Console.WriteLine($"Line 164 IV: '{serv.IV}'");
-                Console.WriteLine($"IV Length: {serv.IV.Length}");
-                Console.WriteLine($"IV Base64 Valid: {IsBase64String(serv.IV)}");
-                byte[] ivBytes = Convert.FromBase64String(serv.IV);
-                Console.WriteLine($"Decoded IV length: {ivBytes.Length}");
 
                 foreach (var kvp in vaultData)
                 {
@@ -176,9 +191,6 @@ namespace lösenordshanterare
             {
                 string clientPath = args[1];
                 string serverPath = args[2];
-                string property = args[3];
-
-
 
                 server serv = JsonSerializer.Deserialize<server>(File.ReadAllText(serverPath));
                 client cl = JsonSerializer.Deserialize<client>(File.ReadAllText(clientPath));
@@ -194,28 +206,26 @@ namespace lösenordshanterare
                 Console.WriteLine(Convert.ToBase64String(vaultKey));
 
                 string decryptedVault;
-
                 decryptedVault = CryptoHelper.Decrypt(encryptedVault, vaultKey, iv);
-
-
                 Dictionary<string, string> vaultData = JsonSerializer.Deserialize<Dictionary<string, string>>(decryptedVault);
 
-                Console.WriteLine($"Line 164 IV: '{serv.IV}'");
-                Console.WriteLine($"IV Length: {serv.IV.Length}");
-                Console.WriteLine($"IV Base64 Valid: {IsBase64String(serv.IV)}");
-                byte[] ivBytes = Convert.FromBase64String(serv.IV);
-                Console.WriteLine($"Decoded IV length: {ivBytes.Length}");
-
-
-                foreach (var kvp in vaultData)
+                if(args.Length == 3)
                 {
-                    if (kvp.Key == property)
-                    {
-                        Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
-                    }
+                    Console.WriteLine("List of properties:");
+                    foreach (var kvp in vaultData)
+                        Console.WriteLine(kvp.Key);
                 }
 
-             //   File.WriteAllText(serverPath, JsonSerializer.Serialize(serv));
+                else if (args.Length == 4)
+                {
+                    string property = args[3];
+
+                    foreach (var kvp in vaultData)
+                    {
+                        if (kvp.Key == property)
+                            Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+                    }
+                }
             }
 
             if (args[0]== "geft")
